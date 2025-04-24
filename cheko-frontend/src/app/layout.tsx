4df3febe30fd199a7386885491e48cec1f1cd5d4
Raneem <>
2025-04-24 18:22:40 +0300

    update frontend and docker compose

--- cheko-frontend/src/app/layout.tsx
+++ cheko-frontend/src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+      <div className="min-h-screen bg-[#F5F6F8]">
+          {children}
+      </div>
       </body>
     </html>
   );
